b974b9efaad0025be08d494a618e4cde00faa6a4
YamiDh99min <62049261+YamiDh99min@users.noreply.github.com>
2020-11-05 17:07:15 -0600

    Update if_else.sh

--- if_else.sh
+++ if_else.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-num_a=100
+num_a=100 // This the Variables 
 num_b=200
 
-if [ $num_a -lt $num_b ]; then
+if [ $num_a -lt $num_b ]; then // num_a is less then num_b 
     echo "$num_a is less than $num_b!"
-fi
+fi // the end if statement 
